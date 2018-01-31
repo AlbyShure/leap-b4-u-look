eae508fe89f6aa5eca4d9855cd2b0e8dca1c2bf5
AlbyShure <allanh03@yahoo.com>
2018-01-30 22:53:47 -0600

    4th iteration more music update

--- leap.js
+++ leap.js
@@ -6,17 +6,62 @@ $('#pause').on('click', function() {
   $('audio')[0].pause();
 });
 
-$('#play-more').on('click', function() {
+$('#play-1').on('click', function() {
   $('#audio-file').attr('src',
 'Song.wav')
 });
 
-$('#play-back').on('click', function() {
+$('#play-2').on('click', function() {
   $('#audio-file').attr('src',
 'Song.mp3')
 });
 
-$('#play-mott').on('click', function() {
+$('#play-3').on('click', function() {
   $('#audio-file').attr('src',
 'Primo.mp3');
+});
+
+$('#play-4').on('click', function () {
+  $('#audio-file').attr('src',
+'Arazu.mp3');
+});
+
+$('#play-5').on('click', function () {
+  $('#audio-file').attr('src',
+'Kintsugi.mp3');
+});
+
+$('#play-6').on('click', function () {
+  $('#audio-file').attr('src',
+'Lilypads.mp3');
+});
+
+$('#play-7').on('click', function () {
+  $('#audio-file').attr('src',
+'zZz.mp3');
+});
+
+$('#play-8').on('click', function () {
+  $('#audio-file').attr('src',
+'Sovereign.mp3');
+});
+
+$('#play-9').on('click', function () {
+  $('#audio-file').attr('src',
+'Memories.mp3');
+});
+
+$('#play-10').on('click', function () {
+  $('#audio-file').attr('src',
+'Runaway (U & I).wav');
+});
+
+$('#play-11').on('click', function () {
+  $('#audio-file').attr('src',
+'Your Soul.mp3');
+});
+
+$('#play-12').on('click', function () {
+  $('#audio-file').attr('src',
+'Falling.wav');
 });
